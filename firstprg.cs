3cbc5c8498b6bec5c36693d2e002779a9a261f6f
Anuprita Rajput <74887568+Anuprita-pro@users.noreply.github.com>
2022-02-08 09:32:26 +0530

    Add files via upload

---
+++ firstprg.cs
@@ -0,0 +1,15 @@
+using System;
+â€5‹
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+      Console.WriteLine(sum); // Print the sum of x + y
+    }
+  }
+}
